4255e6e5aac26da685d71f99669772bfb61d0b77
felipedelosh <doctorfhernandez@hotmail.com>
2024-12-02 17:06:11 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+import { InventoryComponent } from './views/inventory/inventory.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            { path: 'inventario', component: InventoryComponent },
+            { path: 'notfound', component: NotfoundComponent },
+            { path: '**', redirectTo: '/notfound' },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
