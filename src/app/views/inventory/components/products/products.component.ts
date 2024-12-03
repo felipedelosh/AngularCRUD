e8f47a070ea38cbb53d53094698e380b9fe7f527
felipedelosh <doctorfhernandez@hotmail.com>
2024-12-03 13:19:16 -0500

    Iproductt

--- src/app/views/inventory/components/products/products.component.ts
+++ src/app/views/inventory/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/Iproduct';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+
+  arrProduct: Array<IProduct> = [];
+
+  constructor(){}
+
+
+  ngOnInit(){
+
+  }
 
 }
